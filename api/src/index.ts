b684bcd83b56218896ba9c262f28762a6302608b
SachinSilva99 <sachinmanathunga@gmail.com>
2023-12-24 13:03:44 +0530

    connected to mongo db

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import userRoute from "./routes/user.route";
-/*mongoose
-  .connect('')
-  .then(() => console.log("Connected to db"))
-  .catch((er) => console.log(er));*/
+import mongoose from "mongoose";
+import env from "dotenv";
+
+env.config();
+const mongoUrl = process.env.MONGO;
+if (mongoUrl) {
+  mongoose
+    .connect(mongoUrl)
+    .then(() => console.log("Connected to db"))
+    .catch((er) => console.log(er));
+} else {
+  console.error("MongoDB connection URL is undefined. Please check your environment variables.");
+  process.exit(1); // Exit the process with an error code
+}
 
 const app = express();
 
